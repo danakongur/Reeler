57b471a7c72fe73b0f91c894448c64e77fab2497
flamingflarion <jonlogidagbjartsson@gmail.com>
2024-12-03 12:49:19 +0000

    template for battle system
    combat is still missing but it is now turn based.

--- Assets/Scripts/Fisherman.cs
+++ Assets/Scripts/Fisherman.cs
@@ -1,11 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Fisherman_Animator : MonoBehaviour
 {
-    public void LoadMain()
+    public int maxHealth;
+    public int str;
+    public TextMeshProUGUI text;
+    public TextMeshProUGUI debuffText;
+   
+    bool playerTurn = true;
+    int debuff = 0;
+    void Start()
     {
-        Loader.Load(Loader.Scene.Main);
+        int health = maxHealth;
+        text.text = health.ToString() + "/" + maxHealth.ToString();
+        debuffText.text = debuff.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
