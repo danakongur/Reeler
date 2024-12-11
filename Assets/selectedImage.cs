042efba5f2925f80499d80b042edbfb537f41e39
flamingflarion <jonlogidagbjartsson@gmail.com>
2024-12-11 14:21:31 +0000

    added icons for fish behavior

---
+++ Assets/selectedImage.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class selectedImage : MonoBehaviour
+{
+    public Image Attack;
+    public Image Heal;
+    public Image Cleanse;
+    public Image Retreat;
+}
