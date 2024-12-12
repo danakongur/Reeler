c97ca083d474fbd6c6b0d58ae52d5fc2ea56a62d
flamingflarion <jonlogidagbjartsson@gmail.com>
2024-12-12 23:45:58 +0000

    added visible animation to the fishes states

--- Assets/Scripts/FishAttackAnimator.cs
+++ Assets/Scripts/FishAttackAnimator.cs
@@ -7,12 +7,14 @@ public class FishAttackAnimator : MonoBehaviour
 {
     public Vector3 struggleEnd = new Vector3(-5, 0, 0);
     public float struggleDuration = 1;
-    public Vector3 healEnd = new Vector3(-5, 0, 0);
+    public Vector3 healEnd = new Vector3(0, 0, 0);
     public float healDuration = 1;
-    public Vector3 cleanseEnd = new Vector3(-5, 0, 0);
+    public Vector3 cleanseEnd = new Vector3(0, 0, 0);
     public float cleanseDuration = 1;
-    public Vector3 retreatEnd = new Vector3(-5, 0, 0);
+    public Vector3 retreatEnd = new Vector3(5, 0, 0);
     public float retreatDuration = 1;
+    Color healColor = Color.green;
+    Color cleanseColor = Color.cyan;
     /*void OnEnable()
     {
         StartCoroutine(StruggleAnimation());
@@ -29,33 +31,69 @@ public class FishAttackAnimator : MonoBehaviour
             yield return null;
 
         }
+        timer = 0f;
+        while (timer < struggleDuration/2)
+        {
+            timer += Time.deltaTime;
+            transform.localPosition = Vector3.Lerp(struggleEnd, Vector3.zero, timer / (struggleDuration / 2));
+            yield return null;
+
+        }
+
         yield return null;
     }
     public IEnumerator HealAnimation()
     {
         float timer = 0f;
+        SpriteRenderer fishSprite = transform.GetComponent<SpriteRenderer>();
+        Color defaultColor = fishSprite.color; // respects gameobject chosen color
         while (timer < healDuration)
         {
             timer += Time.deltaTime;
-            transform.localPosition = Vector3.Lerp(Vector3.zero, healEnd, timer / healDuration);
+            fishSprite.color = Color.Lerp(defaultColor, healColor, timer / healDuration);
+            //transform.localPosition = Vector3.Lerp(Vector3.zero, healEnd, timer / healDuration);
             yield return null;
 
         }
+        timer = 0f;
+        while (timer < healDuration)
+        {
+            timer += Time.deltaTime;
+            fishSprite.color = Color.Lerp(healColor, defaultColor, timer / healDuration);
+            //transform.localPosition = Vector3.Lerp(Vector3.zero, healEnd, timer / healDuration);
+            yield return null;
+
+        }
+        fishSprite.color = defaultColor;
         yield return null;
     }
     public IEnumerator CleanseAnimation()
     {
         float timer = 0f;
+        SpriteRenderer fishSprite = transform.GetComponent<SpriteRenderer>();
+        Color defaultColor = fishSprite.color; // respects gameobject chosen color
         while (timer < cleanseDuration)
         {
             timer += Time.deltaTime;
-            transform.localPosition = Vector3.Lerp(Vector3.zero, cleanseEnd, timer / cleanseDuration);
+            fishSprite.color = Color.Lerp(defaultColor, cleanseColor, timer / cleanseDuration);
+            //transform.localPosition = Vector3.Lerp(Vector3.zero, healEnd, timer / healDuration);
             yield return null;
 
         }
+        timer = 0f;
+        while (timer < cleanseDuration)
+        {
+            timer += Time.deltaTime;
+            fishSprite.color = Color.Lerp(cleanseColor, defaultColor, timer / cleanseDuration);
+            //transform.localPosition = Vector3.Lerp(Vector3.zero, healEnd, timer / healDuration);
+            yield return null;
+
+        }
+        fishSprite.color = defaultColor;
         yield return null;
     }
-    public IEnumerator RetreatAnimation()
+
+public IEnumerator RetreatAnimation()
     {
         float timer = 0f;
         while (timer < retreatDuration)
